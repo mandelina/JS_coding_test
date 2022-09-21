1bb59e776fb9ac39e5a251dc8da4578f1086f4d7
minju moon <mmj9908@gmail.com>
2022-09-21 18:59:09 +0900

    주차요금계산 답안 보완

--- 프로그래머스/2단계/주차요금계산.js
+++ 프로그래머스/2단계/주차요금계산.js
@@ -46,3 +46,42 @@ function solution(fees, records) {
   }
   return answer;
 }
+
+//다른풀이 ------------------------------------------------------------------------------
+
+function solution(fees, records) {
+  const parkingTime = {}; //{차번호: 시간}으로 저장할 예정
+  records.forEach((r) => {
+    let [time, id, type] = r.split(" ");
+    let [h, m] = time.split(":");
+    time = h * 1 * 60 + m * 1;
+    if (!parkingTime[id]) parkingTime[id] = 0; //차량번호가 저장되지 않았을 경우
+
+    //IN 이면 + (24시간(분) - 입차시간)
+    //OUT이면 -(1439 - 출차시간)
+    if (type === "IN") parkingTime[id] += 1439 - time; //1439는 24 * 60 - 1
+    if (type === "OUT") parkingTime[id] -= 1439 - time;
+  });
+
+  const answer = [];
+  for (let [car, time] of Object.entries(parkingTime)) {
+    if (time <= fees[0]) time = fees[1];
+    else time = Math.ceil((time - fees[0]) / fees[2]) * fees[3] + fees[1]; //기본요금 fee[1] + ( 주차시간 - 기본시간fee[0] ) / fee[2] * fee[3]
+    answer.push([car, time]);
+  }
+  return answer.sort((a, b) => a[0] - b[0]).map((v) => v[1]); // 차량번호 오름차순으로 청구요금 담아 배열로 리턴
+}
+
+//주요코드
+
+// 1. 입출차기록을 정렬할때 메서드체이닝을 사용하여 정렬
+const park = records
+  .map((item) => item.split(" "))
+  .sort((a, b) => (a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]));
+
+//2. 객체를 순회하는방법
+for (let [car, time] of Object.entries(parkingTime)) {
+  if (time <= fees[0]) time = fees[1];
+  else time = Math.ceil((time - fees[0]) / fees[2]) * fees[3] + fees[1]; //기본요금 fee[1] + ( 주차시간 - 기본시간fee[0] ) / fee[2] * fee[3]
+  answer.push([car, time]);
+}
