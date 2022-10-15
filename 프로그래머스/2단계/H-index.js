11637b7b41b2c54c1c425b5646f562908cd9b9b6
minju moon <mmj9908@gmail.com>
2022-10-15 20:19:19 +0900

    1015

---
+++ 프로그래머스/2단계/H-index.js
@@ -0,0 +1,10 @@
+function solution(citations) {
+  let answer = citations;
+  answer.sort((a, b) => b - a);
+
+  for (let i = 0; i < answer.length; i++) {
+    if (i >= answer[i]) return i;
+  }
+
+  return answer.length;
+}
